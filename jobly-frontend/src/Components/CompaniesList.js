4ebb3a4d6c7d5446b25c2d67eba232c18636e32f
Tina Flower <tmflower@gmail.com>
2022-10-04 08:54:40 -1000

    update imports

--- jobly-frontend/src/Components/CompaniesList.js
+++ jobly-frontend/src/Components/CompaniesList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Navigate, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import CompanyCard from "./CompanyCard";
 import JoblyApi from "../api";
 
